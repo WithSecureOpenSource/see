3fd8f8dfd2fee130faca5821c511ae3d228dea3e
Matteo Cafasso <noxdafox@gmail.com>
2016-01-17 00:04:24 +0200

    updated copyright year

--- see/context/context.py
+++ see/context/context.py
@@ -1,4 +1,4 @@
-# Copyright 2015 F-Secure
+# Copyright 2015-2016 F-Secure
 
 # Licensed under the Apache License, Version 2.0 (the "License"); you
 # may not use this file except in compliance with the License.  You may
