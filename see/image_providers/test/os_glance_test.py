ea9942abf0bfbf18c23731e2c133493e71af3d46
Jorge-Rodriguez <jorge.rodriguez@tiriel.eu>
2017-02-14 15:01:27 +0200

    Add glance image checksum verification.

--- see/image_providers/test/os_glance_test.py
+++ see/image_providers/test/os_glance_test.py
@@ -11,7 +11,7 @@ except NameError:
 
 @mock.patch('see.image_providers.GlanceProvider.glance_client')
 @mock.patch('see.image_providers.GlanceProvider.keystone_client')
-@mock.patch('see.image_providers.os_glance.os.path')
+@mock.patch('see.image_providers.os_glance.os')
 class ImageTest(unittest.TestCase):
 
     def setUp(self):
@@ -39,9 +39,9 @@ class ImageTest(unittest.TestCase):
         image.updated_at = '2017-02-14T00:00:00Z'
         glance_mock.images.list.return_value = [image]
 
-        os_mock.exists.return_value = True
-        os_mock.isfile.return_value = True
-        os_mock.getmtime.return_value = 32503680000.0  # Jan 1st, 3000
+        os_mock.path.exists.return_value = True
+        os_mock.path.isfile.return_value = True
+        os_mock.path.getmtime.return_value = 32503680000.0  # Jan 1st, 3000
 
         resources = Resources('foo', self.config)
         expected_image_path = self.config['disk']['image']['provider_configuration']['target_path']
@@ -60,45 +60,95 @@ class ImageTest(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             _ = resources.provider_image
 
-    @mock.patch('__builtin__.open')
-    def test_stale_image_exists(self, open_mock, os_mock, keystone_mock, glance_mock):
+    @mock.patch('__builtin__.open', new_callable=mock.mock_open)
+    @mock.patch('see.image_providers.os_glance.hashlib')
+    def test_stale_image_exists(self, hashlib_mock, open_mock, os_mock, keystone_mock, glance_mock):
         image = mock.MagicMock()
         image.id = '1'
+        image.checksum = '1111'
         image.name = 'TestImageName'
         image.updated_at = '2017-02-14T00:00:00Z'
         glance_mock.images.list.return_value = [image]
 
-        os_mock.exists.return_value = True
-        os_mock.isfile.return_value = True
-        os_mock.isdir.return_value = False
-        os_mock.getmtime.return_value = 0
+        md5 = mock.MagicMock()
+        md5.hexdigest.return_value = '1111'
+        hashlib_mock.md5.return_value = md5
+
+        os_mock.path.exists.return_value = True
+        os_mock.path.isfile.return_value = True
+        os_mock.path.isdir.return_value = False
+        os_mock.path.getmtime.return_value = 0
 
         resources = Resources('foo', self.config)
         expected_image_path = self.config['disk']['image']['provider_configuration']['target_path']
 
         assert resources.provider_image == expected_image_path
         glance_mock.images.data.assert_called_with('1')
-        open_mock.assert_called_with(expected_image_path, 'wb')
+        self.assertTrue([mock.call(expected_image_path, 'wb'),
+                         mock.call(expected_image_path, 'rb')],
+                        open_mock.call_args_list)
+        os_mock.remove.assert_not_called()
 
-    @mock.patch('__builtin__.open')
-    def test_newer_image_exists(self, open_mock, os_mock, keystone_mock, glance_mock):
+    @mock.patch('__builtin__.open', new_callable=mock.mock_open)
+    @mock.patch('see.image_providers.os_glance.hashlib')
+    def test_same_name_images_exist(self, hashlib_mock, open_mock, os_mock, keystone_mock, glance_mock):
         image1 = mock.MagicMock()
         image1.id = '1'
+        image1.checksum = '1111'
         image1.name = 'TestImageName'
         image1.updated_at = u'2017-02-14T00:00:00Z'
         image2 = mock.MagicMock()
         image2.id = '2'
+        image2.checksum = '2222'
         image2.name = 'TestImageName'
         image2.updated_at = u'2017-02-15T00:00:00Z'
         glance_mock.images.list.return_value = [image1, image2]
 
-        os_mock.exists.return_value = True
-        os_mock.isfile.return_value = False
-        os_mock.isdir.return_value = True
+        md5 = mock.MagicMock()
+        md5.hexdigest.return_value = '2222'
+        hashlib_mock.md5.return_value = md5
+
+        os_mock.path.exists.return_value = True
+        os_mock.path.isfile.return_value = False
+        os_mock.path.isdir.return_value = True
 
         resources = Resources('foo', self.config)
         expected_image_path = self.config['disk']['image']['provider_configuration']['target_path'] + '/2'
 
         assert resources.provider_image == expected_image_path
         glance_mock.images.data.assert_called_with('2')
-        open_mock.assert_called_with(expected_image_path, 'wb')
+        self.assertTrue([mock.call(expected_image_path, 'wb'),
+                         mock.call(expected_image_path, 'rb')],
+                        open_mock.call_args_list)
+        os_mock.remove.assert_not_called()
+
+    @mock.patch('__builtin__.open', new_callable=mock.mock_open)
+    @mock.patch('see.image_providers.os_glance.hashlib')
+    def test_checksum_mismatch(self, hashlib_mock, open_mock, os_mock, keystone_mock, glance_mock):
+        image = mock.MagicMock()
+        image.id = '1'
+        image.checksum = '1111'
+        image.name = 'TestImageName'
+        image.updated_at = '2017-02-14T00:00:00Z'
+        glance_mock.images.list.return_value = [image]
+
+        md5 = mock.MagicMock()
+        md5.hexdigest.return_value = '1234'
+        hashlib_mock.md5.return_value = md5
+
+        os_mock.path.exists.return_value = True
+        os_mock.path.isfile.return_value = True
+        os_mock.path.isdir.return_value = False
+        os_mock.path.getmtime.return_value = 0
+
+        resources = Resources('foo', self.config)
+        expected_image_path = self.config['disk']['image']['provider_configuration']['target_path']
+
+        with self.assertRaisesRegexp(RuntimeError, 'Checksum failure. File: '):
+            assert resources.provider_image == expected_image_path
+
+        glance_mock.images.data.assert_called_with('1')
+        self.assertTrue([mock.call(expected_image_path, 'wb'),
+                         mock.call(expected_image_path, 'rb')],
+                        open_mock.call_args_list)
+        os_mock.remove.assert_called_once_with(expected_image_path)
