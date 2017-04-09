36337aeeefd1d6c8a324f69bf86a6ffd7c54ce10
Matteo Cafasso <noxdafox@gmail.com>
2017-04-09 23:28:22 +0300

    plugins: fix timers hook
    JSON does not allow integers as keys.
    
    Ensure timers are daemon threads.
    
    Clear timers on cleanup.
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- plugins/timers.py
+++ plugins/timers.py
@@ -28,7 +28,7 @@ class TimersHook(Hook):
     configuration::
 
         {
-          "timers": {40: "event_to_trigger_after_40_seconds"}
+          "timers": {"event_to_trigger_after_40_seconds": 40}
         }
 
     Timers are started during Hook's initialization.
@@ -37,5 +37,14 @@ class TimersHook(Hook):
     def __init__(self, parameters):
         super().__init__(parameters)
 
-        for time, event in self.configuration.get('timers', {}).items():
-            Timer(time, self.context.trigger, args=(event, )).start()
+        self.timers = tuple(
+            Timer(t, self.context.trigger, args=(e, ))
+            for e, t in self.configuration.get('timers', {}).items())
+
+        for timer in self.timers:
+            timer.daemon = True
+            timer.start()
+
+    def cleanup(self):
+        for timer in self.timers:
+            timer.cancel()
