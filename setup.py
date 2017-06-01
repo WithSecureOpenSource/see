536893581c90987d22d55d35ee86a0641d3e8b73
Matteo Cafasso <noxdafox@gmail.com>
2017-06-01 13:24:08 +0300

    release 1.2.5
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(fname):
 
 setup(
     name="python-see",
-    version="1.2.4",
+    version="1.2.5",
     author="F-Secure Corporation",
     author_email="matteo.cafasso@f-secure.com",
     description=("Sandboxed Execution Environment"),
