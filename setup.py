a9b30aa1f90b264413ed4ecf5ebebc1ef0b732f2
Jorge-Rodriguez <jorge.rodriguez@tiriel.eu>
2020-04-03 09:50:09 +0300

    Bump revision

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(fname):
 
 setup(
     name="python-see",
-    version="1.3.5",
+    version="1.3.6",
     author="F-Secure Corporation",
     author_email="matteo.cafasso@f-secure.com",
     description=("Sandboxed Execution Environment"),
